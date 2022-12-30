a2cf3570d51fc08d71bc86f27c4d8ab3cefa86a7
seaHour <74128686+seaHour@users.noreply.github.com>
2022-12-29 18:29:47 -0800

    fixup

--- client/py/setup.py
+++ client/py/setup.py
@@ -4,20 +4,20 @@ from setuptools import setup, find_namespace_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(install_requires=['grpcio==1.51.1', 'protobuf==4.21.12'],
-      name="series",
-      version=f"{os.getenv('BUILD_MAJOR', 0)}.{os.getenv('BUILD_MINOR', 0)}.{os.getenv('BUILD_PATCH', 0)}",
-      author="tester",
-      description="Project Testing >_",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://dummy.io/",
-      packages=find_namespace_packages(where="src"),
-      package_dir={"": "src"},
-      include_package_data=True,
-      classifiers=[
-    "Programming Language :: Python :: 3",
-    "License :: Other/Proprietary License",
-],
+setup(
+    name="series",
+    version=f"{os.getenv('BUILD_MAJOR', 0)}.{os.getenv('BUILD_MINOR', 0)}.{os.getenv('BUILD_PATCH', 0)}",
+    author="tester",
+    description="Project Testing >_",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://dummy.io/",
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+    ],
     python_requires='>=3.9',
 )
